527fea31518f9c53cbeb8700d99a7037e9eeb98b
Sam Jeon <samueljeon99@gmail.com>
2021-11-16 20:53:32 -0500

    bubble sort

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-# print(None is not None)
+# print(None is not None)\
+import sys
 
 hashtable = {}
 hashtable[0] = "world"
@@ -8,4 +9,8 @@ print( hashtable.get(0))
 num = None
 x = num or 0;
 y = not True
-print(x)
+# print(None < 10)
+
+arr = [1,2,3,4,5]
+for num in range(len(arr)-1):
+    print(arr[num])
