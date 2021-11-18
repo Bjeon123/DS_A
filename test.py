bfacb3f14f290f482fa8f4c10e7c5ff274164360
Sam Jeon <samueljeon99@gmail.com>
2021-11-17 21:01:25 -0500

    reformat folder

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 # print(None is not None)\
+# import math
+# arr = list(range(1,10))
+# min = min(arr)
+# max = max(arr)
+# numOccur = arr.count(3)
+# str = "Hello World"
+# upcase = str.upper();
+# downcase = str.lower();
+
+# print(math.ceil(45.3))
+# print(5/2)
+# print(numOccur)
 import sys
 
 hashtable = {}
