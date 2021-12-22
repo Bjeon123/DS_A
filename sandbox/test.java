d3fabd3b28e089a9fe82d2fbed7aa14387b37686
Sam Jeon <samueljeon99@gmail.com>
2021-12-22 14:52:49 -0500

    java review

--- sandbox/test.java
+++ sandbox/test.java
@@ -1,8 +1,37 @@
-class Solution{
+import java.io.Console;
 
+class Solution{
     public static void main(String[] args) {
-        Integer[] foo = new Integer[3];
-        foo[0]= 1;
-        System.out.println(foo);
+        // primitive types
+        //boolean
+        boolean bool = true;
+
+        // int
+        int i = 100000;
+        int intMin = Integer.MIN_VALUE;
+        //-2147483648
+        int intMax = Integer.MAX_VALUE;
+        //2147483647
+
+        // byte
+        byte byteMin = Byte.MIN_VALUE;
+        //-128
+        byte byteMax = Byte.MAX_VALUE;
+        //127
+
+        //short
+        short shortMin = Short.MIN_VALUE;
+        //-32768
+        short shortMax = Short.MAX_VALUE;
+        //32767
+
+        //long
+        long l = 2147483648L;
+        long longMin = Long.MIN_VALUE;
+        //-9223372036854775808
+        long longMax = Long.MAX_VALUE;
+        //9223372036854775807
+
+        System.out.println(cast);
     }
 }
